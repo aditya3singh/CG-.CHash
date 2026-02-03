17e00c25087c7bfe8abf5ee849cbeb75d7fb4f8c
aditya3singh <singhaditya20030@gmail.com>
2026-02-03 22:42:16 +0530

    Mahirl and Alphabets and Vowels

---
+++ TopBrainCSharp/MahirlAndAlphabetsAndVowels/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        string str1 = Console.ReadLine();
+        string str2 = Console.ReadLine();
+        string result = "";
+        for(int i= 0; i< str1.Length; i++)
+        {
+            char ch = char.ToLower(str1[i]);
+            if (IsVowel(ch))
+            {
+                result += str1[i];
+            }
+            else
+            {
+                if (!str2.ToLower().Contains(ch))
+                {
+                    result += str2[i];
+                }
+            }
+        }
+        string finalResult = "";
+        for(int i = 0; i < result.Length; i++)
+        {
+            if (i == 0 || result[i] != result[i - 1])
+            {
+                finalResult += result[i];
+            }
+        }
+        Console.WriteLine(finalResult);
+    }
+
+    static bool IsVowel(char ch)
+    {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+}
