8d9697e56d313f3585fc16b9b14a017316bc58ce
aditya3singh <singhaditya20030@gmail.com>
2026-02-03 23:01:21 +0530

    Inventory Name Cleanup

---
+++ TopBrainCSharp/InventoryNameCleanup/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.Tracing;
+
+class Program
+{
+    public static void Main()
+    {
+        string str = Console.ReadLine();
+        str = str.Trim();
+        string result = "";
+
+        for(int i = 0; i< str.Length; i++)
+        {
+            if(i > 0 && str[i] == str[i - 1])
+            {
+                continue;
+            }
+            if (i == 0 || str[i - 1] == ' ')
+            {
+                result += char.ToUpper(str[i]);
+            }
+            else
+            {
+                result += char.ToLower(str[i]);
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
